fdfd37a8065229f3bdc888b2241ff940d3715acf
Sherfin S <sherfin94@gmail.com>
2016-07-12 17:36:16 +0530

    Remove test of screen clearing check

--- spec/terminal_ui_spec.rb
+++ spec/terminal_ui_spec.rb
@@ -1,7 +1,7 @@
 require 'terminal_ui'
 
 describe TerminalUI do
-  let(:ui) { TerminalUI.new}
+  let(:ui) { TerminalUI.new }
 
   it 'responds to display_board' do
     expect(ui).to respond_to(:display_board)
@@ -9,18 +9,5 @@ describe TerminalUI do
 
   it 'responds to get_next_move' do
     expect(ui).to respond_to(:get_next_move)
-  end
-
-  context 'after a move is made' do
-    after(:example) do
-
-      ui.get_next_move
-    end
-
-    it 'clears the screen' do
-
-      expect(ui).to receive(:clear_screen)
-
-    end
   end
 end
