c99e746a495ca177f9d29f91cb1d31c06a5a57d1
Sherfin S <sherfin94@gmail.com>
2016-07-12 15:28:55 +0530

    Add test to check some squares after move P e2-e4

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,16 +1,20 @@
 require 'board'
 
 describe Board do
-  it 'has 64 squires' do
+  it 'has 64 squares' do
     expect(Board.new.number_of_squares).to eq(64)
   end
 
-  it 'should respond a call for resetting it' do
+  it 'responds to a call for resetting it' do
     expect(Board.new).to respond_to(:reset)
   end
 
-  it 'should have number of pieces between 0 and 32' do
+  it 'has number of pieces between 0 and 32' do
     expect(Board.new.number_of_pieces).to be_between(0, 32)
+  end
+
+  it 'responds to request for making moves' do
+    expect(Board.new).to respond_to(:move)
   end
 
   context 'upon reset' do
@@ -19,20 +23,38 @@ describe Board do
       board.reset
     end
 
+    it 'has next move given to white' do
+      expect(board.next_move).to eq("W")
+    end
+
     it 'number of pieces should be equal to 32' do
       expect(board.number_of_pieces).to eq(32)
     end
 
-    it 'should have the square b4 empty' do
+    it 'has the square b4 empty' do
       expect(board['b4']).to eq('E')
     end
 
-    it 'should have the square d1 with the white queen' do
+    it 'has the square d1 with the white queen' do
       expect(board['d1']).to eq('Q')
     end
 
-    it 'should have the square c8 with the black bishop' do
+    it 'has the square c8 with the black bishop' do
       expect(board['c8']).to eq('b')
+    end
+
+    context 'upon move e4' do
+      before(:example) do
+        board.move('P e2 e4')
+      end
+
+      it 'has the square e2 empty' do
+        expect(board['e2']).to eq('E')
+      end
+
+      it 'has the square e2 with a white pawn' do
+        expect(board['e4']).to eq('P')
+      end
     end
   end
 end
