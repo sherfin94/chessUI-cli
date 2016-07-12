2c5369682f5c8e27b848552bd97eea543470f0ea
Sherfin S <sherfin94@gmail.com>
2016-07-12 17:05:16 +0530

    Implement TerminalUI class

--- lib/terminal_ui.rb
+++ lib/terminal_ui.rb
@@ -1,2 +1,31 @@
+require_relative 'board'
+
 class TerminalUI
+  def initialize
+    @board = Board.new
+    while true
+      display_board
+      move = get_next_move
+      @board.move(move)
+    end
+  end
+
+  def display_board
+    @board.squares.each_with_index do |square, index|
+      print "#{square} " unless square == 'E'
+      print "  " if square == "E"
+      print "\n" if (index+1)%8 == 0
+    end
+  end
+
+  def get_next_move
+    print "#{@board.next_move}'s move : "
+    move = gets
+    clear_screen
+    move
+  end
+
+  def clear_screen
+    system 'clear'
+  end
 end
