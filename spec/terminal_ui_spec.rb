d4f3fb73a1e4e7da4c13eabab2fcda708aba0c7a
Sherfin S <sherfin94@gmail.com>
2016-07-12 17:05:43 +0530

    Add tests for TerminalUI

--- spec/terminal_ui_spec.rb
+++ spec/terminal_ui_spec.rb
@@ -1,6 +1,26 @@
 require 'terminal_ui'
 
 describe TerminalUI do
-  it 'responds to display_board'
-  it 'responds to get_next_move'
+  let(:ui) { TerminalUI.new}
+
+  it 'responds to display_board' do
+    expect(ui).to respond_to(:display_board)
+  end
+
+  it 'responds to get_next_move' do
+    expect(ui).to respond_to(:get_next_move)
+  end
+
+  context 'after a move is made' do
+    after(:example) do
+
+      ui.get_next_move
+    end
+
+    it 'clears the screen' do
+
+      expect(ui).to receive(:clear_screen)
+
+    end
+  end
 end
